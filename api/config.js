a3f9f90947ba432a1844610210dc01109a977fad
sepiropht <sergembotta@mailoo.org>
2019-03-08 11:24:50 +0100

    reboot!

--- api/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  "database": 'mongodb://root:abc123@ds011291.mlab.com:11291/todoist-clone',
-  "port": process.env.PORT || 4000,
-  "secretKey": 'azerty'
-}
--- api/public/js/app.js
+++
@@ -1,7 +0,0 @@
-var routerApp = angular.module('todo', ['ui.router']);
-
-routerApp.config(function($stateProvider, $urlRouterProvider) {
-
-    $urlRouterProvider.otherwise('/');
-
-});
