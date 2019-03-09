fca049fb4b7f79908a7a6669648b3b0d63f315f2
sepiropht <sergembotta@mailoo.org>
2019-03-09 11:33:27 +0100

    init layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,23 @@
-import React from 'react'
-import App from '../app/index'
+import React from "react";
+import styled from "styled-components";
 
-export default () => <App></App>
+const ToolBar = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 50px;
+  top: 0;
+  background-color: red;
+`;
+const Container = styled.div`
+  width: auto;
+  font-size: 13px;
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  padding: 0 !important;
+`;
+export default () => (
+  <Container>
+    <ToolBar />
+  </Container>
+);
